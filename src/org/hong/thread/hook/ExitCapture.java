cfd9589f834f7ddc0496995c3c71e2d4fa0b6fc4
hong <1043518275@qq.com>
2017-11-26 20:45:37 +0800

    高并发实战之向Java虚拟机注册一个shutdown钩子事件，一旦程序结束事件到来时，就运行线程hook

---
+++ src/org/hong/thread/hook/ExitCapture.java
@@ -0,0 +1,40 @@
+package org.hong.thread.hook;
+
+
+/**
+ * @author hong
+ * @version v1.1
+ * @ClassName: ExitCapture
+ * @Description: (程序异常退出或者kill pid, 捕获并执行逻辑块.)
+ * @date 2017/11/26
+ */
+public class ExitCapture {
+
+    public static void main(String[] args) {
+
+        // 在jvm中增加一个关闭的钩子，当jvm关闭的时候，会执行系统中已经设置的所有通过方法addShutdownHook添加的钩子，
+        // 当系统执行完这些钩子后，jvm才会关闭。
+        Runtime.getRuntime().addShutdownHook(new Thread(()->{
+            System.out.println("the application will be exit.");
+            notityAddRelease();
+        }));
+
+        int i = 0;
+        while (true) {
+            try {
+                Thread.sleep(1_000);
+                System.out.println("I am  working...");
+            } catch (InterruptedException e) {
+                //e.printStackTrace();
+                //ignore
+            }
+            i++;
+            if (i >= 20) throw new RuntimeException("error");
+        }
+
+    }
+
+    private static void notityAddRelease() {
+        System.out.println("will release resource(socket,file,connection...)");
+    }
+}
